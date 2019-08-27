254f2aeb54a069c1d96b9481efe9f2b2da41b4c5
Alexandr <it_np@mail.ru>
2019-08-27 18:49:27 +0600

    003

--- Udemy/Less_001/start.py
+++ Udemy/Less_001/start.py
@@ -4,7 +4,6 @@ print('start Py')
 # 2.
 print("I'm a student and I'll become a \"strong\" programmer") #I'm a student and I'll become a "strong" programmer
 print("C:\\Users\\GoodStudent") #C:\Users\GoodStudent
-
 x = "My name is Agent Smith"
 print(x[1]) #y
 print(x[3:17:3]) #nesgt
